e178412dba114641f599ff3772b0c9ce37c7bede
Yuko Uda <yuko.uda0110@gmail.com>
2020-05-23 13:26:16 -0700

    Adjust eslint configs and fix errors

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,41 +1,41 @@
-var db = require("../models");
+var db = require('../models')
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
+  app.get('/', function (req, res) {
+    db.Example.findAll({}).then(function (dbExamples) {
+      res.render('index', {
+        msg: 'Welcome!',
         examples: dbExamples
-      });
-    });
-  });
+      })
+    })
+  })
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
+  app.get('/example/:id', function (req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function (
       dbExample
     ) {
-      res.render("example", {
+      res.render('example', {
         example: dbExample
-      });
-    });
-  });
+      })
+    })
+  })
 
   // Lang and Culture Test
-  app.get("/lang-culture", function(req, res) {
-    console.log(req.headers["accept-language"]);
-    res.render("lang", {
-      lang: req.headers["accept-language"]
-    });
-  });
+  app.get('/lang-culture', function (req, res) {
+    console.log(req.headers['accept-language'])
+    res.render('lang', {
+      lang: req.headers['accept-language']
+    })
+  })
 
-  app.post("/lang-culture", function() {
-    console.log("post test");
-  });
+  app.post('/lang-culture', function () {
+    console.log('post test')
+  })
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
-  });
-};
+  app.get('*', function (req, res) {
+    res.render('404')
+  })
+}
--- server.js
+++ server.js
@@ -1,49 +1,49 @@
-require("dotenv").config();
-var morgan = require("morgan");
-var express = require("express");
-var exphbs = require("express-handlebars");
+require('dotenv').config()
+var morgan = require('morgan')
+var express = require('express')
+var exphbs = require('express-handlebars')
 
-var db = require("./models");
+var db = require('./models')
 
-var app = express();
-var PORT = process.env.PORT || 3000;
+var app = express()
+var PORT = process.env.PORT || 3000
 
 // Middleware
-app.use(morgan("dev"));
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(express.static("public"));
+app.use(morgan('dev'))
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(express.static('public'))
 
 // Handlebars
 app.engine(
-  "handlebars",
+  'handlebars',
   exphbs({
-    defaultLayout: "main"
+    defaultLayout: 'main'
   })
-);
-app.set("view engine", "handlebars");
+)
+app.set('view engine', 'handlebars')
 
 // Routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require('./routes/apiRoutes')(app)
+require('./routes/htmlRoutes')(app)
 
-var syncOptions = { force: false };
+var syncOptions = { force: false }
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
-if (process.env.NODE_ENV === "test") {
-  syncOptions.force = true;
+if (process.env.NODE_ENV === 'test') {
+  syncOptions.force = true
 }
 
 // Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
+db.sequelize.sync(syncOptions).then(function () {
+  app.listen(PORT, function () {
     console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      '==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.',
       PORT,
       PORT
-    );
-  });
-});
+    )
+  })
+})
 
-module.exports = app;
+module.exports = app
