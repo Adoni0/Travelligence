0eedc6fd05b2b156c41a31bd5d33d22913970daf
Adam SOudani <asoudani@email.com>
2020-05-25 11:45:25 -0700

    added logic to wealth algo

---
+++ models/userProfile.js
@@ -0,0 +1,27 @@
+module.exports = function (sequelize, DataTypes) {
+    var Profile = sequelize.define('Profile', {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoInrement: true,
+        primaryKey: true
+      },  
+      location: DataTypes.STRING,
+      wealth: DataTypes.DECIMAL(1,5),
+      cost_bracket: DataTypes.STRING, //luxury, moderate, economy
+      culture: DataTypes.STRING,
+      language: DataTypes.STRING,
+      interests: DataTypes.DECIMAL(1, 5),
+      somethingNew: DataTypes.BOOLEAN,
+    })
+    return Profile
+  };
+  
+//   Profile {
+// 	location: ____,
+// 	wealth: ______,
+// 	culture: ______,
+// 	lang: ______,
+// 	interests: _____,
+// 	something_new: _______
+// }
