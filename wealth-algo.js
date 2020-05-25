2b72e7e3b15c7771095337dce0de9b4ac15b4d71
Yuko Uda <yuko.uda0110@gmail.com>
2020-05-25 14:37:42 -0700

    Fix lint error

--- wealth-algo.js
+++ wealth-algo.js
@@ -1,76 +1,75 @@
-var geoip = require('geoip-lite');
-var MediumIncome = require('./mediumIncome.js');
-var db = require('./models');
+var geoip = require('geoip-lite')
+var MediumIncome = require('./mediumIncome.js')
+var db = require('./models')
 
-var ip = '207.97.227.239';
-var geo = geoip.lookup(ip);
+var ip = '207.97.227.239'
+var geo = geoip.lookup(ip)
 
 // console.log(geo);
 
 for (var i = 0; i < MediumIncome.length; i++) {
   if (geo.region === MediumIncome[i].location) {
     // console.log(MediumIncome[i].income)
-    var income = MediumIncome[i].income;
+    var income = MediumIncome[i].income
     // find age of user express as a decimal
     // add two decimals together and divide by 2 to find average
-    var age = 0.26;
-    var wealth = (income + age) / 2;
-    console.log(wealth); //wealth decimal
+    var age = 0.26
+    var wealth = (income + age) / 2
+    console.log(wealth) // wealth decimal
   } else {
     console.log('Could not find your state')
   }
 };
 
-if (wealth <= .33333) {
-  //cost bracket = economy
+if (wealth <= 0.33333) {
+  // cost bracket = economy
   db.Profile.update({
     cost_bracket: 'Economy'
   },
-    {
-      where: {
-        id: require.body.id
-      }
+  {
+    where: {
+      id: require.body.id
     }
+  }
   ).catch(function (error) {
     if (error) throw error
-  });
-} else if (wealth <= .66666 && wealth > .33333) {
-  //cost bracket = moderate
+  })
+} else if (wealth <= 0.66666 && wealth > 0.33333) {
+  // cost bracket = moderate
   db.Profile.update({
     cost_bracket: 'Moderate'
   },
-    {
-      where: {
-        id: require.body.id
-      }
+  {
+    where: {
+      id: require.body.id
     }
+  }
   ).catch(function (error) {
     if (error) throw error
-  });
+  })
 } else {
-  //cost bracket = luxury
+  // cost bracket = luxury
   db.Profile.update({
     cost_bracket: 'Luxury'
   },
-    {
-      where: {
-        id: require.body.id
-      }
+  {
+    where: {
+      id: require.body.id
     }
+  }
   ).catch(function (error) {
     if (error) throw error
-  });
+  })
 }
-
 
 db.Profile.create({
   location: geo.region,
   wealth: wealth,
 
   where: {
-    id: require.body.id //have to give value of {{id}} to section of handlebars
+    id: require.body.id // have to give value of {{id}} to section of handlebars
   }
 
 }).catch(function (error) {
   if (error) throw error
-});
+})
