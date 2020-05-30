c90d1a74efcea6f13032eb36c1a35419cdedbcb7
Adam SOudani <asoudani@email.com>
2020-05-30 12:26:42 -0700

    force false

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.get(
 require('./routes/apiRoutes')(app)
 require('./routes/htmlRoutes')(app)
 
-var syncOptions = { force: true }
+var syncOptions = { force: false }
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
